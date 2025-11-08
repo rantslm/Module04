e74350d82b2164d10ef79e0e3a9cb547fe1dbb44
rantslm <leilanirants@gmail.com>
2025-11-08 17:56:28 -0500

    update

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-function addCard() {
+const data = [
+  { name: 'bob', age: 23 },
+  { name: 'alice', age: 39 },
+];
+
+function addCard(data) {
+  console.log(data);
   // clone the template
   const template = document
     .getElementById('card-template')
     .content.cloneNode(true);
 
   // populate the template
-  template.querySelector('.card-title').innerText = 'My Card Title';
-  template.querySelector('.card-text').innerText = 'lorem ipsum ble bla';
+  template.querySelector('.card-title').innerText = data.name;
+  template.querySelector('.card-text').innerText = data.age;
 
   // include the populated template into the page
   document.querySelector('#card-list').appendChild(template);
 }
 
-addCard();
+//addCard(data.forEach);
+//data.forEach(element => addCard(element))
+
+for (element of data) {addCard(element)}
